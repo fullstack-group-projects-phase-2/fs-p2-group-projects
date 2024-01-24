312c57df550cc8de2d22385758174022fc8aecee
ELvan Surahmat <146643689+ElvanSurahmat@users.noreply.github.com>
2024-01-24 11:26:47 +0700

    merge new update from development setup

---
+++ client/src/components/LoginView.jsx
@@ -0,0 +1,60 @@
+export default function LoginView() {
+  return (
+    <>
+      <section className="h-screen bg-[#295de7] text-white p-4">
+        <div className="container h-full px-6 py-24">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between lg:ml-10">
+            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+              <img
+                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="w-full"
+                alt="Phone image"
+              />
+            </div>
+
+            <div className="md:w-8/12 lg:ml-[6rem] lg:w-5/12">
+              <div className="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  type="text"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="exampleFormControlInput3"
+                  placeholder="Email address"
+                />
+                <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
+                  Email address
+                </label>
+              </div>
+
+              <div class="relative mb-6" data-te-input-wrapper-init>
+                <input
+                  type="password"
+                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                  id="exampleFormControlInput33"
+                  placeholder="Password"
+                />
+                <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
+                  Password
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="bg-white text-black font-medium p-4 rounded-full text-lg hover:shadow-2xl hover:bg-blue-500  hover:text-white transition duration-200 ease-in-out w-full"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Sign in
+              </button>
+
+              <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+                  OR
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        dark: "#1a202c",
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
