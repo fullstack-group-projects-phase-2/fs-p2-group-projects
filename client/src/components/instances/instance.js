eb05503d2ce175d0481fb529930535d635d68de9
Arma Yoga Pratama <mgarmayogapratama@gmail.com>
2024-01-25 12:38:37 +0700

    setup client deploy

--- client/src/components/instances/instance.js
+++ client/src/components/instances/instance.js
@@ -1,8 +1,8 @@
-import axios from axios
+import axios from "axios";
 
 const instance = axios.create({
   // baseURL: 'https://localhost:3000',
-  baseURL: 'https://d-talk.mgarmayogapratama.xyz',
+  baseURL: "https://d-talk.mgarmayogapratama.xyz",
 });
 
-export default instance
+export default instance;
